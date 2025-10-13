1b6d5f4bfebfdb4d2e818682e6c14e2b942dfefe
Ylan <turin-ylan@outlook.fr>
2025-10-13 13:51:12 +0200

    fe

--- commands/utility/newinscription.js
+++ commands/utility/newinscription.js
@@ -35,7 +35,31 @@ module.exports = {
         collector.on('collect', async i => {
             await i.deferUpdate();
             const selected = i.values[0];
-            await i.editReply({ content: `Vous avez sélectionné la classe : ${selected}`, components: [] });
+            await i.editReply({ content: `Vous avez sélectionné la classe : ${selected}. Maintenant refaites le ?`, components: [row2] });
+            // Here you can handle the selected value, e.g., save it to a database
+            console.log(`User selected: ${selected}`);
+            text = `New inscription for class: ${selected
+            }`;
+        });
+        collector.on('end', collected => {
+            if (collected.size === 0) {
+                interaction.editReply({ content: 'Vous n\'avez pas sélectionné de classe à temps.', components: [] });
+            }
+        });
+        const menu2 = new StringSelectMenuBuilder()
+            .setCustomId('select2')
+            .setPlaceholder('Sélectionnez votre classe...')
+            .setMinValues(1)
+            .setMaxValues(1)
+            .addOptions(classe.map(c => new StringSelectMenuOptionBuilder(c)));
+        const row2 = new ActionRowBuilder()
+            .addComponents(menu2);
+        const filter2 = i => i.customId === 'select2' && i.user.id === interaction.user.id;
+        const collector2 = interaction.channel.createMessageComponentCollector({ filter2, componentType: ComponentType.StringSelect, time: 60000, max: 1 });
+        collector2.on('collect', async i => {
+            await i.deferUpdate();
+            const selected = i.values[0];
+            await i.editReply({ content: `Vous avez sélectionné la classe : ${selected}. Bravo !`, components: [] });
             // Here you can handle the selected value, e.g., save it to a database
             console.log(`User selected: ${selected}`);
             text = `New inscription for class: ${selected
@@ -46,6 +70,5 @@ module.exports = {
                 interaction.editReply({ content: 'Vous n\'avez pas sélectionné de classe à temps.', components: [] });
             }
         });
-        await interaction.reply({ content: `Inscription créée : ${text}`, components: [] });
     }
 };
