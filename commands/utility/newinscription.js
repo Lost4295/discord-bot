ae626d20a2a512223260a627e18089ac17815fad
Ylan <turin-ylan@outlook.fr>
2025-10-13 13:53:52 +0200

    im testing

--- commands/utility/newinscription.js
+++ commands/utility/newinscription.js
@@ -29,6 +29,15 @@ module.exports = {
             .addOptions(classe.map(c => new StringSelectMenuOptionBuilder(c)));
         const row = new ActionRowBuilder()
             .addComponents(menu);
+        const menu2 = new StringSelectMenuBuilder()
+            .setCustomId('select2')
+            .setPlaceholder('Sélectionnez votre classe...')
+            .setMinValues(1)
+            .setMaxValues(1)
+            .addOptions(classe.map(c => new StringSelectMenuOptionBuilder(c)));
+        const row2 = new ActionRowBuilder()
+            .addComponents(menu2);
+
         await interaction.reply({ content: 'Sélectionnez votre classe dans le menu ci-dessous :', components: [row] });
         const filter = i => i.customId === 'select' && i.user.id === interaction.user.id;
         const collector = interaction.channel.createMessageComponentCollector({ filter, componentType: ComponentType.StringSelect, time: 60000, max: 1 });
@@ -46,18 +55,10 @@ module.exports = {
                 interaction.editReply({ content: 'Vous n\'avez pas sélectionné de classe à temps.', components: [] });
             }
         });
-        const menu2 = new StringSelectMenuBuilder()
-            .setCustomId('select2')
-            .setPlaceholder('Sélectionnez votre classe...')
-            .setMinValues(1)
-            .setMaxValues(1)
-            .addOptions(classe.map(c => new StringSelectMenuOptionBuilder(c)));
-        const row2 = new ActionRowBuilder()
-            .addComponents(menu2);
+
         const filter2 = i => i.customId === 'select2' && i.user.id === interaction.user.id;
         const collector2 = interaction.channel.createMessageComponentCollector({ filter2, componentType: ComponentType.StringSelect, time: 60000, max: 1 });
         collector2.on('collect', async i => {
-            await i.deferUpdate();
             const selected = i.values[0];
             await i.editReply({ content: `Vous avez sélectionné la classe : ${selected}. Bravo !`, components: [] });
             // Here you can handle the selected value, e.g., save it to a database
