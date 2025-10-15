8d475540f570b5b7297e36f791551a665f1e47e1
Ylan <turin-ylan@outlook.fr>
2025-10-15 02:07:50 +0200

    remake pts, now needs testing

--- commands/everyone/pts.js
+++ commands/everyone/pts.js
@@ -6,94 +6,117 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('getpts')
 		.setDescription('Pour voir les points obtenus')
-		// .addStringOption(option => option.setName('semestre').setDescription("Quel Semestre vous aimeriez voir.").setChoices(
-		// 	{ value: "1", name: "Semestre 1" },
-		// 	{ value: "2", name: "Semestre 2" },
-		//).setRequired(true))
+		.addStringOption(option => option.setName('trimestre').setDescription("Quel trimestre vous aimeriez voir.").setChoices(
+			{ value: "1", name: "Trimestre 1" },
+			{ value: "2", name: "Trimestre 2" },
+			{ value: "3", name: "Trimestre 3" },
+		).setRequired(true))
 		.addUserOption(option => option.setName('user').setDescription('L\'utilisateur dont vous voulez voir les points.')),
 	async execute(interaction) {
 		const pool = require("../../db.js");
-		
-		await interaction.reply("Cette commande ne fonctionne pas pour l'instant, mais elle le sera très vite ! Demandez à <@349983254373466114> pour avoir la réponse à votre demande.");
-		return;
+		// await interaction.reply("Cette commande ne fonctionne pas pour l'instant, mais elle le sera très vite ! Demandez à <@349983254373466114> pour avoir la réponse à votre demande.");
 		pool.getConnection(async function (err, connection) {
 			const user = interaction.options.getUser('user') ?? interaction.user;
-			const semestre = interaction.options.getString('semestre');
-			let exampleEmbed = new EmbedBuilder()
-				.setColor(0x0099FF)
-				.setThumbnail(user.avatarURL())
-				.setTimestamp()
-				.setFooter({ text: 'Couch Bot' });
-			const member = interaction.guild.members.cache.get(interaction.user.id);
-			await interaction.deferReply();
-			let query;
-			console.log(semestre);
-			if (semestre == "1") {
-				query = 'SELECT pseudo, points, visibility, is_admin FROM users where id =';
-			} else if (semestre == "2") {
-				query = 'SELECT pseudo, points_s2, visibility, is_admin FROM users where id =';
+			const trimestre = interaction.options.getString('trimestre');
+			switch (trimestre) {
+				case "1":
+					fq = "SELECT date_debut, date_fin FROM trimestre WHERE trimestre = 1 AND niveau = '?';";
+					break;
+				case "2":
+					fq = "SELECT date_debut, date_fin FROM trimestre WHERE trimestre = 2 AND niveau = '?';";
+					break;
+				case "3":
+					fq = "SELECT date_debut, date_fin FROM trimestre WHERE trimestre = 3 AND niveau = '?';";
+					break;
+				default:
+					await interaction.editReply({ content: "Le trimestre doit être 1, 2 ou 3." });
+					return;
 			}
-			connection.query(query + user.id, async function (error, results) {
+			connection.query('SELECT classe as niveau,pseudo,visibility,is_admin FROM users WHERE id = ?', [user.id], async function (error, theuser) {
 				if (error) throw error;
-				console.log(results);
-				if (results.length == 0) {
+				if (theuser.length == 0) {
 					await interaction.editReply({ content: user.username + ' n\'est inscrit dans la base de données de Couch Bot. ' });
-				} else if (results[0].visibility == 0 && interaction.user.id != user.id && !member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+					return;
+				}
+				if (theuser[0].classe == null) {
+					await interaction.editReply({ content: user.username + ' n\'a pas de classe enregistrée dans la base de données de Couch Bot. ' });
+					return;
+				}
+				if (theuser[0].visibility == 0 && interaction.user.id != user.id && !member.permissions.has(PermissionsBitField.Flags.Administrator)) {
 					await interaction.editReply({ content: user.username + ' a choisi de ne pas rendre ses points visibles.' });
-				} else {
+					return;
+				}
+				niveau = theuser[0].niveau;
+				connection.query(fq, [niveau], async function (error, dates) {
+					if (error) throw error;
+					if (dates.length == 0) {
+						await interaction.editReply({ content: 'Il n\'y a pas de dates enregistrées pour le trimestre ' + trimestre + ' et le niveau "' + niveau + '".' });
+						return;
+					}
+					// select sum(points.points) as Total, user_id from points group by user_id;
+					// SELECT u.id AS user_id, COALESCE(SUM(p.points), 0) AS total_points FROM users u LEFT JOIN points p ON p.user_id = u.id     AND p.date BETWEEN NOW()- INTERVAL 2 DAY AND NOW()  -- ta condition de date WHERE u.id = 1 GROUP BY u.id;
+					let exampleEmbed = new EmbedBuilder()
+						.setColor(0x0099FF)
+						.setThumbnail(user.avatarURL())
+						.setTimestamp()
+						.setFooter({ text: 'Couch Bot' });
+					await interaction.deferReply();
+					console.log(trimestre);
+					if (error) throw error;
+					console.log(results);
 					let desc;
 					if (interaction.user.id != user.id) {
-						desc = results[0].pseudo + ' a';
+						desc = theuser[0].pseudo + ' a';
 					} else {
 						desc = 'Vous avez';
 					}
-					desc += ' actuellement ' + (semestre == 2 ? results[0].points_s2 : results[0].points) + ' points.';
-					if (semestre == 1) {
-						if (results[0].points >= 4 && results[0].is_admin == 0) {
+
+					exampleEmbed.setTitle('Points obtenus par ' + theuser[0].pseudo);
+
+					let query2 = "SELECT u.id AS user_id,COALESCE(SUM(p.points), 0) AS total_points FROM users u LEFT JOIN points p ON p.user_id = u.id AND p.date BETWEEN ? AND ? WHERE u.id = ? GROUP BY u.id";
+					connection.query(query2, [startDate, endDate, user.id], async function (error, mypts) {
+
+						desc += ' actuellement ' + mypts[0].total_points + ' points.';
+
+						if (mypts[0].total_points >= 4 && theuser[0].is_admin == 0) {
 							desc += " Votre note sera cependant limitée à 4 points.";
-						} else if (results[0].points >= 6 && results[0].is_admin == 1) {
+						} else if (mypts[0].total_points >= 6 && theuser[0].is_admin == 1) {
 							desc += " Votre note sera cependant limitée à 6 points.";
 						}
-					} else {
-						if (results[0].points_s2 >= 4 && results[0].is_admin == 0) {
-							desc += " Votre note sera cependant limitée à 4 points.";
-						} else if (results[0].points_s2 >= 6 && results[0].is_admin == 1) {
-							desc += " Votre note sera cependant limitée à 6 points.";
-						}
-					}
-					exampleEmbed.setTitle('Points obtenus par ' + results[0].pseudo);
-					exampleEmbed.setDescription(desc);
-					let query2;
-					if (semestre == "1") {
-						query2 = 'SELECT * FROM points_2024 where user_id = '
-					} else if (semestre == "2") {
-						query2 = 'SELECT * FROM points_s2_2024 where user_id = '
-					}
-					connection.query(query2 + user.id, async function (error, results) {
+
+						desc+= " La fin de ce trimestre est le " + dates[0].date_fin + ".";
+
+						exampleEmbed.setDescription(desc);
+
 						if (error) throw error;
-						console.log(results);
-						if (results.length == 0) {
-							exampleEmbed.addFields({ name: 'Points', value: 'Vous n\'avez pas de points.', inline: true });
-						} else {
-							for (let i = 0; i < results.length; i++) {
-								if (i <= 23) {
-									if (results[i].points < 0) {
-										exampleEmbed.addFields({ name: 'Perte de Points', value: results[i].points * -1 + " points perdus le " + results[i].date + " pour la raison suivante : " + results[i].reason, inline: true });
+						let query3 = "SELECT points, reason, date FROM points WHERE user_id = ? AND date BETWEEN ? AND ? ORDER BY date DESC";
+						connection.query(query3, [user.id, startDate, endDate], async function (error, results) {
+							if (error) throw error;
+							console.log(results);
+							if (results.length == 0) {
+								exampleEmbed.addFields({ name: 'Points', value: 'Vous n\'avez pas de points.', inline: true });
+							} else {
+								for (let i = 0; i < results.length; i++) {
+									if (i <= 23) {
+										if (results[i].points < 0) {
+											exampleEmbed.addFields({ name: 'Perte de Points', value: results[i].points * -1 + " points perdus le " + results[i].date + " pour la raison suivante : " + results[i].reason, inline: true });
+										} else {
+											exampleEmbed.addFields({ name: 'Gain de Points', value: results[i].points + " points gagnés le " + results[i].date + " pour la raison suivante : " + results[i].reason, inline: true });
+										}
 									} else {
-										exampleEmbed.addFields({ name: 'Gain de Points', value: results[i].points + " points gagnés le " + results[i].date + " pour la raison suivante : " + results[i].reason, inline: true });
+										exampleEmbed.addFields({ name: 'Autres', value: 'Les autres actions ne sont pas visibles. Pour en avoir un aperçu, veuillez exécuter la commande /allactions.', inline: true });
+										break;
 									}
-								} else {
-									exampleEmbed.addFields({ name: 'Autres', value: 'Les autres actions ne sont pas visibles. Pour en avoir un aperçu, veuillez exécuter la commande /allactions.', inline: true });
-									break;
 								}
 							}
-						}
-						await interaction.editReply({ embeds: [exampleEmbed] });
-					});
-				}
-			});
-			pool.releaseConnection(connection);
-		});
+							await interaction.editReply({ embeds: [exampleEmbed] });
+						});
 
+					}
+					)
+				});
+			});
+		});
+		pool.releaseConnection(connection);
 	}
-};
+}
