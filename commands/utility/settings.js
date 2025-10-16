8d357df542668f5432ffdec0cf82a7807e6cdd1f
Ylan <turin-ylan@outlook.fr>
2025-10-16 21:18:38 +0200

    z

--- commands/utility/settings.js
+++ commands/utility/settings.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, MessageFlags, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags, StringSelectMenuBuilder, StringSelectMenuOptionBuilder,ActionRowBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
